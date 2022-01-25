7f89514b6a970191c78ad9859c0c63452a22610e
RodrigoMac <rodrigo_macrt@hotmail.com>
2022-01-25 14:06:07 -0300

    SQL Exercise

--- main.py
+++ main.py
@@ -2,38 +2,56 @@
 
 import pika
 import logging
+import pyodbc
+
+def on_message(channel, method, properties, body):
+    logging.info(f'Received message: {body}')
+    if body == b'stop':
+        raise KeyboardInterrupt("")
+
+cnxn = connection = pyodbc.connect(f'DRIVER=ODBC Driver 17 for SQL Server;SERVER=localhost\SQLSERVER,1433;DATABASE=Treinamento;UID=espada;PWD=@espada')
+
+cursor = cnxn.cursor()
+
+cursor.execute("insert into Message(id, text, timestamp) values (203, 'ALOW', 1972-12-10 18:41:39)")
 
 FORMAT = '%(asctime)s %(message)s'
 logging.basicConfig(format=FORMAT, level=logging.INFO)
 
-# SUPRIME LOG DE INFORMACOES DO PIKA
 logging.getLogger('pika').propagate = False
 
 # DEFINE OS PARAMETROS DE CONEXAO COM O RABBITMQ Credentials(usuario, senha); Parameters('localhost', 5672, '/', credentials)
-credentials = pika.PlainCredentials('espa', '@espada')
+credentials = pika.PlainCredentials('espada', '@espada')
 parameters = pika.ConnectionParameters('localhost',
                                        5672,
                                        '/',
                                        credentials)
 
-# IMPRIME LOG DE INFORMACOES
 logging.info('Creating connection')
 
-
+# CRIA CONEXAO COM SERVIDOR DO RABBITMQ
 try:
-    # CRIA CONEXAO COM SERVIDOR DO RABBITMQ
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
 
+    # BASIC GET
+    #method, properties, body = channel.basic_get('TesteRodrigo', auto_ack=True)
+
+    # CONSUME
+    channel.basic_consume('TesteRodrigo', on_message, auto_ack=True)
+
+    channel.start_consuming()
+
     # CRIA UMA FILA DE TESTE NO RABBITMQ
-    channel.queue_declare('TesteRodrigo')
+    # channel.queue_declare('TesteRodrigo')
 
     # DEFAUTL EXCHANGE: ENVIA MENSAGEM PARA FILA COM NOME IGUAL A ROUTING KEY
-    channel.basic_publish('', 'TesteRodrigo', 'ALOW3')
+    #channel.basic_publish('', 'TesteRodrigo', 'ALOW2')
 
     # ENCERRA CONEXAO
+
+except KeyboardInterrupt:
+    logging.info("Get KeyboardInterrupt")
+    channel.stop_consuming()
     connection.close()
 
-except pika.exceptions.ProbableAuthenticationError as error:
-    # IMPRIME MENSAGEM DE ERRO
-    logging.error(error)
